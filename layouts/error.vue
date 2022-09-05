b8c21b0f80d95a8fca0818f0bc21be8508abf919
Zac <a19860101@gmail.com>
2022-09-05 20:33:50 +0800

    20220905

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="text-center container">
     <h1 v-if="error.statusCode === 404" class="text-5xl font-bold p-6">Page not found</h1>
+    <h1 v-if="error.statusCode === 503" class="text-5xl font-bold p-6">{{error.message}}</h1>
     <h1 v-else>An error occurred</h1>
     <NuxtLink to="/" class="hover:text-blue-400 underline">Home page</NuxtLink>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <button @click="increment()">+</button>
-    <div>{{counter}}</div>
+    <div>{{ counter }}</div>
+    <div v-for="todo in todos" :key="todo.id">{{ todo.text }}</div>
+    <div>{{ error }}</div>
     <!-- <div>{{todoUnComplete}}</div> -->
   </div>
 </template>
@@ -13,6 +15,40 @@ export default {
   name: 'IndexPage',
   head: {
     title: '首頁'
+  },
+  // asyncData(context){
+  //   return context.$axios.get('http://localhost:3333/todos').then(res => {
+  //     return {
+  //       todos: res.data
+  //     }
+  //   });
+  // },
+  // computed:{
+
+  // asyncData({$axios}){
+  //   return $axios.get('http://localhost:3333/todos').then(res => {
+  //     return {
+  //       todos: res.data
+  //     }
+  //   }).catch(e=>{
+  //     return {
+  //       error: e,
+  //     }
+  //   });
+  // },
+
+  async asyncData({$axios,error}){
+    try{
+      const res = await $axios.get('http://localhost:3333/todos');
+      return {
+        todos: res.data
+      }
+    }catch(e){
+      error({
+        statusCode: 503,
+        message: 'unable to get data'
+      })
+    }
   },
   computed:{
     // spread operator
