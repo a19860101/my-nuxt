84d9b67f6d2a53bd70b4b35b574fa2308cf3cd4c
Zac <a19860101@gmail.com>
2022-09-23 21:19:02 +0800

    20220923

--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -2,8 +2,12 @@
   <div>
 
     <h1>Product #{{this.$route.params.id}}</h1>
+    <h2>{{product.name}}</h2>
     <div>
-      {{product}}
+      <form action="" method="post">
+        <input type="text" v-model="name" class="border border-gray-400 p-2">
+        <input type="submit" value="儲存" class="bg-blue-300 px-5 py-2" @click.prevent="updateProduct(product.id)">
+      </form>
       <form action="" method="post">
         <input type="submit" value="刪除" class="bg-red-300 px-5 py-2" @click.prevent="deleteProduct(product.id)">
       </form>
@@ -12,12 +16,22 @@
   </div>
 </template>
 <script>
-  import { mapState } from 'vuex';
+  import { get } from 'http';
+import { mapState } from 'vuex';
   export default {
+    data(){
+      return {
+        name:''
+      }
+    },
     computed:{
-      ...mapState({
-        product: state => state.product.product,
-      })
+      // ...mapState({
+      //   product: state => state.product.product,
+      // })
+      product(){
+        this.name = this.$store.state.product.product.name;
+        return this.$store.state.product.product
+      }
     },
     methods:{
       deleteProduct(id){
@@ -26,6 +40,17 @@
           this.$nuxt.$loading.start();
           alert('資料已刪除');
           this.$router.push('/products')
+        })
+      },
+      updateProduct(id){
+        this.$axios.put('http://localhost:3333/data/'+id,{
+          name: this.name
+        })
+        .then(res=>{
+          this.$nuxt.$loading.start();
+          alert('資料已更新');
+          // this.$router.push('/products/'+id)
+          window.location.href='/products/'+id
         })
       }
     },
